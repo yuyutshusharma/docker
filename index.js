9063bc400344c1760a0172374f98fee79f577dae
Yuyutshu Sharma <yuyutshucsit22@oic.edu.np>
2026-03-25 12:49:34 +0545

    Update greeting message in root endpoint

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello From Yuyutshu" }');
+    res.send('{ "response": "Hello From Yuyutshu Sharma" }');
 });
 
 app.get('/will', function (req, res) {
